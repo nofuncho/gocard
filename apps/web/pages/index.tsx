5a55dc6ec58d9d4baa1eb96b51242905d13abda7
nofuncho <nofun.cho@gmail.com>
2025-09-17 00:31:18 +0900

    chore: apply .gitignore and normalize line endings

---
+++ apps/web/pages/index.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>🚀 GOcard MVP v0.1</h1>
+      <p>Hello World! (Next.js + Turborepo)</p>
+    </div>
+  );
+}
